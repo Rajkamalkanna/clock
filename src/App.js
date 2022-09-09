1f1cc4a84933a76819f4076a4e45c465417c889d
rajkamalkanna4 <rajkamalkanna4@gmail.com>
2022-09-09 21:50:09 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import "./styles.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+export default function App() {
+   const date = new Date()
+   const hours = date.getHours()
+   let timeOfDay
+   if(hours < 12){
+    timeOfDay = "Moring"
+   }else if(hours >=12 && hours < 17){
+    timeOfDay ="Afternoon"
+   }else if(hours >=17 && hours < 22){
+    timeOfDay = "Evening"
+   }else{
+    timeOfDay = "Night"
+   }
+    return(
+        <div className='time'>
+            <h1>Good <span className='wish'>{timeOfDay}</span></h1>
+            <h2>{Date()}</h2>
+        </div>
+    )
+};
